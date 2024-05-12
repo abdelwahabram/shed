4966b2849dd799435534d693441494e3428ef929
abdo <63767622+abdelwahabram@users.noreply.github.com>
2024-05-12 22:26:26 +0300

    created add_file() method, used File class to perform this operation

--- source/under_construction_area.py
+++ source/under_construction_area.py
@@ -1,5 +1,6 @@
 from directory_path import DirectoryPath
 from current_portal import CurrentPortal
+from file import File
 from pathlib import Path
 
 from tree import Tree
@@ -49,9 +50,51 @@ class UnderConstructionArea:
         return True
         
 
-    def add_file(self):
-        pass
+    def add_file(self, file_path):
+        
+        if self.directory_path.get_path() == None:
+            print("error404: repo not found")
+            return False
+        
+        file_object = File(directory_path = self.directory_path.get_path(), path = file_path)
+        
+        file_object.create()
+        
+        hash_value = file_object.get_hash()
+        
+        file_name = file_object.get_name()
+        
+        if file_name not in self.new_shell:
+            self.new_shell[file_name] = {"hash": hash_value, "mode":100644, "status": "created"}
+            self.write_area()
+            print("200:file tracked successfully")
+            return True
+        
+        if hash_value == self.new_shell[file_name]["hash"]:
+            print("no changes detected")
+            return True
+        
+        if file_name in self.current_shell:
+            
+            if hash_value == self.current_shell[file_name]["hash"]:
+                
+                self.new_shell[file_name]["hash"] = hash_value
+                self.new_shell[file_name]["status"] = "no change"
+                self.write_area()
+                
+                print("changes undone")
+                
+                return True
+        
+        
+        self.new_shell[file_name] = {"hash":hash_value, "mode": 100644, "status": "modified"}
+        self.write_area()
+        
+        print("200: updates was tracked successfully")
+        
+        return True
 
+                
     def build(self):
         pass
 
@@ -70,3 +113,12 @@ class UnderConstructionArea:
 
 B = UnderConstructionArea()
 B.create()
+test_path = Path("source/tree.py")
+print(test_path.exists())
+print(B.current_shell)
+print(B.new_shell)
+
+print("/////////////////////////////////////")
+B.add_file(test_path)
+print(B.current_shell)
+print(B.new_shell)

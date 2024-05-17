d1df6ff528e97b3a939709fd300a929370f8792b
abdo <63767622+abdelwahabram@users.noreply.github.com>
2024-05-17 16:53:41 +0300

    1. created build() method to build the new shell 2. created read_area(), write_area() methods to read/write the area data to the disk 3. tested the new functionalities maually and fixed a couple of bugs

--- source/under_construction_area.py
+++ source/under_construction_area.py
@@ -1,10 +1,16 @@
 from directory_path import DirectoryPath
 from current_portal import CurrentPortal
 from file import File
-from pathlib import Path
-
+from user import User
+from shell import Shell
 from tree import Tree
+
+from pathlib import Path
+import json
+
+
 class UnderConstructionArea:
+    
     def __init__(self):
 
         self.current_shell = {}
@@ -16,6 +22,8 @@ class UnderConstructionArea:
         self.area_path = self.__set_area_path()
 
         self.current_portal = CurrentPortal(self.directory_path.get_path())
+        
+        self.user = User(directory_path = self.directory_path.get_path())
     
 
     def create(self):
@@ -95,15 +103,73 @@ class UnderConstructionArea:
         return True
 
                 
-    def build(self):
-        pass
+    def build(self, message):
+        
+        if self.directory_path.get_path() == None:
+            print("error404: repo not found")
+            return False
+        
+        is_changed = False
+        for file_meta_data in self.new_shell.values():
+            if file_meta_data["status"] != "no change":
+                is_changed = True
+                break
+        
+        if not is_changed:
+            print("no changes lately")
+            return False
+        
+        new_shell_tree = Tree(name = "", directory_path = self.directory_path.get_path(), path = self.directory_path.get_path())
+        
+        for file_name, file_meta_data in self.new_shell.items():
+            
+            file_hash_ = file_meta_data["hash"]
+            file_path = self.directory_path.get_path().joinpath(f"{file_name}").relative_to(self.directory_path.get_path())
+            
+            new_shell_tree.iterate_path(file_path, file_hash_)
+        
+        new_shell_tree.create()
+        new_shell_tree_hash = new_shell_tree.get_hash()
+        
+        current_shell_tree_hash = self.current_portal.get_current_shell_hash()
+        
+        new_shell = Shell(message = message, tree_hash = new_shell_tree_hash, parent_hash = current_shell_tree_hash, user = self.user, directory_path = self.directory_path.get_path())
 
+        new_shell.create()
+        
+        new_shell_hash = new_shell.get_hash()
+        
+        self.current_portal.set_current_pointer(new_shell_hash)
+        
+        self.area_path.unlink()
+        
+        self.create()
+        
+        print("200: updates was built successfully")
+        
+        return True
+
+    
     def write_area(self):
-        pass
+        area_object = {"current_shell": self.current_shell, "new_shell": self.new_shell}
 
+        with open(self.area_path, "w") as json_handle:
+            json.dump(area_object, json_handle)
+        
+        return True
+    
+    
     def read_area(self):
-        pass
+        with open(self.area_path, "r") as json_handle:
+            area_object = json.load(json_handle)
 
+        self.current_shell = area_object["current_shell"]
+        
+        self.new_shell = area_object["new_shell"]
+        
+        return True
+
+    
     def __set_area_path(self):
         if self.directory_path.get_path() == None:
             return None
@@ -113,12 +179,13 @@ class UnderConstructionArea:
 
 B = UnderConstructionArea()
 B.create()
-test_path = Path("source/tree.py")
-print(test_path.exists())
-print(B.current_shell)
-print(B.new_shell)
+test_path = Path("source/user.py")
+# print(test_path.exists())
+# print(B.current_shell)
+# print(B.new_shell)
 
 print("/////////////////////////////////////")
 B.add_file(test_path)
-print(B.current_shell)
-print(B.new_shell)
+# print(B.current_shell)
+# print(B.new_shell)
+B.build("jhj")

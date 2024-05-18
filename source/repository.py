adab17ce8ea9c1cb54bedd1ffa32c7babcacbe78
abdo <63767622+abdelwahabram@users.noreply.github.com>
2024-05-18 22:22:04 +0300

    1.integrated UnderConstructionArea and other class in the repository 2. tested the updates manually

--- source/repository.py
+++ source/repository.py
@@ -1,15 +1,21 @@
 from directory_path import DirectoryPath
+from under_construction_area import UnderConstructionArea
+from user import User
 from pathlib import Path
 import os
 
 
 class Repository:
     def __init__(self):
-        self.directory_path = DirectoryPath().get_path()
-        self.underConstructionArea = None
+        self.directory_path = DirectoryPath()
+        
+        if DirectoryPath().get_path() != None:
+            self.user = User(directory_path = self.directory_path.get_path())
+            self.under_construction_area = UnderConstructionArea(directory_path = self.directory_path, user = self.user)
+    
     
     def create(self):
-        if self.directory_path != None:
+        if self.directory_path.get_path() != None:
             print("error403: repo already exists")
             return False
         
@@ -24,23 +30,28 @@ class Repository:
         with open(".shed/CUR_PORTAL", "w") as current_portal:
             current_portal.write("ptr: ptrs/portals/master\n")
         
-        self.directory_path = Path()
-        print(self.directory_path)
+        # self.directory_path = Path()
+        # print(self.directory_path)
+        self.directory_path.set_path(Path())
+        
+        self.user = User(directory_path = self.directory_path.get_path())
+        self.under_construction_area = UnderConstructionArea(directory_path = self.directory_path, user = self.user)
         
         print("200: ##repository created## ")
 
         return True
 
-    def add_file(self):
-        pass
 
-    def build(self):
-        pass
+    def add_file(self, file_path):
+        self.under_construction_area.create()
+        return self.under_construction_area.add_file(file_path)
+
+
+    def build(self, message):
+        self.under_construction_area.create()
+        return self.under_construction_area.build(message)
+
 
     def show_status(self):
-        pass
-
-# A= Repository()
-
-# A.create()
-# print(A.directory_path)
+        self.under_construction_area.create()
+        return self.under_construction_area.show_status()
